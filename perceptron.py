794e3d78921b9cca9e5f49023578a19d67856627
AlessioBorzi <alessio520@hotmail.it>
2023-05-03 16:37:53 +0200

    feat: perceptron algorithm

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,15 @@
 import numpy
 import pandas
 
-#def perceptron(csv_file, t):
-    
-data = pandas.read_csv("data.csv")
-d = data.shape[1]-1 # space dimension
-n = data.shape[0] # number of points
-theta = numpy.zeros(d)
-for i in range(n):
-    r = 
-
-        
+def perceptron(csv_file, T):  
+    for t in range(T):
+        data = pandas.read_csv(csv_file)
+        d = data.shape[1]-1 # space dimension
+        n = data.shape[0] # number of points
+        theta = numpy.zeros(d)
+        for i in range(n):
+            x = data.iloc[i, 0:d]
+            y = data.iloc[i, d]
+            if y * numpy.dot(x, theta) <= 0:
+                theta += y*x
+    return theta
